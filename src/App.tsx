6355db53bffd1e8431ec912f70a5b240f3b71727
Tarek HAMMAMI <tarek.hammami@wecraft.tn>
2023-01-17 14:04:10 +0100

    feat: solve get current window tabs and open them in a new one challenges

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import _styles from "./App.scss"
+import { useEffect, useState } from 'react';
+
+import _styles from './App.scss';
 
 const App = () => {
+  // const [currWinTabs, setCurrWinTabs] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const currWin = await chrome.windows.getCurrent();
+
+      const tabs = await chrome.tabs.query({});
+      const currWinTabs = tabs
+        .filter((tab) => tab.windowId === currWin.id)
+        .map(({ id, favIconUrl, title, url }) => ({
+          id,
+          favIconUrl,
+          title,
+          url,
+        }));
+
+      chrome.windows.create({
+        state: 'maximized',
+        url: currWinTabs.map((tab) => tab.url) as string[],
+      });
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Chrome Tabs Saver</h1>
+      <h1>Chrome Tabs SFSFS</h1>
     </div>
   );
 };
